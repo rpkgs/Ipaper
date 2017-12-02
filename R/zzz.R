90d1bc2cf6f3da0c2fba249e1dcbba47e226501f
孔冬冬 <kongdd@mail2.sysu.edu.cn>
2017-12-03 00:50:40 +1100

    update makeVIDEO

--- R/zzz.R
+++ R/zzz.R
@@ -39,12 +39,19 @@ setwd_clip <- function() setwd(getwd_clip())
 
 #' @title makeVIDEO
 #' @description make video through ffmpeg
+#' 
+#' @param file filename of output video
+#' @param path The path of figures, i.e, pngs, to make video
+#' @param pattern regular expression pattern to filter figures
+#' @param mode must be one of "ultrafast", "slow", "veryslow"
+#' 
 #' @export
-makeVIDEO <- function(file = "ffmpeg_%d.avi", path, 
+makeVIDEO <- function(file = "ffmpeg_%d.avi", 
+                      path = ".", pattern = "*.png",
                       mode = c("ultrafast", "slow", "veryslow")){
   mode <- mode[1]
-  shell(sprintf("(for %%i in (%s\\*.png) do @echo file '%%i') > list.txt", 
-                gsub("/", "\\\\", path)))
+  shell(sprintf("(for %%i in (%s\\%s) do @echo file '%%i') > list.txt", 
+                gsub("/", "\\\\", path), pattern))
   shell(sprintf("ffmpeg -safe 0 -f concat -r 2 -i list.txt -c:v libx264 -preset %s -crf 0 %s", 
                 mode, gsub("/", "\\\\", file)))
   file.remove("list.txt") #remove list.txt, return null
