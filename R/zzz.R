7fbe6beb81e023d73e05ab4158204d955e1a0047
孔冬冬 <kongdd@mail2.sysu.edu.cn>
2017-12-10 15:34:19 +1100

    add running Id function

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,23 @@
 #' @import httr xml2 magrittr
 NULL
 
+windowsFonts(
+  Times = windowsFont("Times New Roman"), 
+  ST    = windowsFont("宋体"), 
+  YH    = windowsFont("Microsoft Yahei"), 
+  Arial = windowsFont("Arial"))
+
+#' pal
+#' show colors in figure device
+#' @export
+pal <- function(col, border = "light gray")
+{
+    n <- length(col)
+    plot(0, 0, type="n", xlim = c(0, 1), ylim = c(0, 1), 
+         axes = FALSE, xlab = "", ylab = "")
+    rect(0:(n-1)/n, 0, 1:n/n, 1, col = col, border = border)
+}
+
 #' @title dir.show
 #' @description open assign path in windows explorer, and default path is 
 #' current directory. This function is only designed for windows system.
@@ -22,6 +39,12 @@ dir.show <- function(path = getwd()){
 #' @description print sprintf result into console just like C style fprintf function
 #' @export
 fprintf <- function(fmt, ...) cat(sprintf(fmt, ...))
+
+#' @title runningId
+#' @export
+runningId <- function(i, step = 1) {
+    if (mod(i, step) == 0) cat(sprintf("running %d ...\n", i))
+}
 
 #' @title getwd_clip
 #' @description get directory path in clipboard, same as getwd function
@@ -53,7 +76,7 @@ makeVIDEO <- function(file = "ffmpeg_%d.avi",
   shell(sprintf("(for %%i in (%s\\%s) do @echo file '%%i') > list.txt", 
                 gsub("/", "\\\\", path), pattern))
   shell(sprintf("ffmpeg -safe 0 -f concat -r 2 -i list.txt -c:v libx264 -preset %s -crf 0 %s", 
-                mode, gsub("/", "\\\\", file)))
+                mode, gsub("/", "\\\\", file) ))
   file.remove("list.txt") #remove list.txt, return null
 }
 
