070aabbde366f59086d82ea4d5ba03fdf240c152
Dongdong Kong <kongdd@users.noreply.github.com>
2019-05-15 18:52:52 +0800

    fix document

--- R/melt_list.R
+++ R/melt_list.R
@@ -7,6 +7,16 @@
 #' @param na.rm Boolean
 #' @param ... other parameters to melt
 #' 
+#' @examples
+#' # data.frame
+#' df <- data.frame(year = 2010, day = 1:3, month = 1, site = "A")
+#' l  <- list(a = df, b = df)
+#' df_new <- melt_list(l, "id")
+#' 
+#' # data.table
+#' df <- data.table::data.table(year = 2010, day = 1:3, month = 1, site = "A")
+#' l  <- list(a = df, b = df)
+#' df_new <- melt_list(l, "id")
 #' @importFrom reshape2 melt
 #' @importFrom data.table is.data.table
 #' @export
