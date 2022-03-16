43e8e6be519b779c271e1fd32ddbdacdb5471f67
Dongdong Kong <kongdd@users.noreply.github.com>
2022-03-16 23:09:33 +0800

    add `prepend_path`

---
+++ R/module.R
@@ -0,0 +1,11 @@
+#' @export
+prepend_path <- function (NAME, VALUE, head = FALSE) {
+  vals = Sys.getenv(NAME)
+  vals = if (vals == "") 
+    VALUE
+  else {
+    if (head) paste0(vals, ":", VALUE) else paste0(VALUE, ":", vals)
+  }
+  cmd = sprintf("Sys.setenv(%s=vals)", NAME)
+  eval(parse(text = cmd))
+}
