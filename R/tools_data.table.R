ddbb2c53ffa39eaaf84d229a8e40cc0838d1c2f2
Dongdong Kong <kongdd@users.noreply.github.com>
2023-10-25 18:29:40 +0800

    add fread_dir

---
+++ R/tools_data.table.R
@@ -0,0 +1,20 @@
+#' fread_dir
+#' 
+#' @param ... others to [data.table::fread()]
+#' 
+#' @importFrom data.table rbindlist data.table
+#' @export
+fread_dir <- function(indir, pattern = "*.csv", ..., list2df=TRUE) {
+  fs = dir(indir, pattern, full.names = TRUE)
+  fs = set_names(fs, basename(fs))
+
+  res = lapply(fs, fread, ...)
+  if (!list2df) return(res)
+
+  tryCatch({
+    rbindlist(res)
+  }, error = function(e) {
+    message(sprintf('%s', e$message))
+    res
+  })
+}
