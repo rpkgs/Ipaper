61b634b51d9c9007ab01267cc45179308e67915d
Dongdong Kong <kongdd@users.noreply.github.com>
2024-03-22 18:59:50 +0800

    add date_ydn, date_yj

--- R/tools_date.R
+++ R/tools_date.R
@@ -6,3 +6,16 @@ date_ym <- function(date) make_date(year(date), month(date))
 #' @export
 #' @rdname date_ym
 date_y <- function(date) make_date(year(date))
+
+#' @export
+#' @rdname date_ym
+date_yj <- function(year, doy) {
+  make_date(year) + doy - 1
+}
+
+#' @export
+#' @rdname date_ym
+date_ydn <- function(year, dn, delta = 8) {
+  doy = (dn - 1) * delta + 1
+  make_date(year) + doy - 1
+}
