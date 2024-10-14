acf7b1b9b5c5e83a10ff1ac1ef84b2fe3d75dfa9
Dongdong Kong <kongdd@users.noreply.github.com>
2024-10-14 17:47:19 +0800

    add `read_ufile` and `write_ufile`

--- R/tools_data.table.R
+++ R/tools_data.table.R
@@ -18,3 +18,30 @@ fread_dir <- function(indir, pattern = "*.csv", ..., .progress="text", list2df=T
     res
   })
 }
+
+#' @export
+read_ufile <- function(f, nodata = c(-99999, -9999)) {
+  unit <- fread(f, nrows = 1)
+  dat <- fread(f, skip = 2, header = FALSE)
+  for (nd in nodata) {
+    dat[dat == nd] <- NA
+  }
+  names(dat) <- names(unit)
+  structure(list(data = dat, unit = unit), class = "unit_df")
+}
+
+# define a S3 method
+#' @export
+write_ufile <- function(x, ...) UseMethod("write_ufile")
+
+#' @export
+write_ufile.data.frame <- function(data, unit, fout) {
+  fwrite(unit, fout)
+  fwrite(data, fout, col.names = FALSE, append = TRUE)
+}
+
+#' @export
+write_ufile.unit_df <- function(l, fout) {
+  fwrite(l$unit, fout)
+  fwrite(l$data, fout, col.names = FALSE, append = TRUE)
+}
